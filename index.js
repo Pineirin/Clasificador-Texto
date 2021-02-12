293ddfd1f7ad9e974b08f0e83ab5f0d2f5426ae2
davillafer <davillafer@gmail.com>
2021-02-12 20:03:13 +0100

    Shows solution

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ app.use(express.static(__dirname + '/public'));
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded());
 
 app.get('/', function (_, res) {
     res.render('home');
 });
 
-app.post('/transform', function (_, res) {
-    res.render('solution');
+app.post('/transform', function (req, res) {
+    res.render('solution', {solution: req.body.textArea});
 });
 
 app.listen(3000, (_req, _res) => {
